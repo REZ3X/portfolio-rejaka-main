50aef614330cd24474daa7f6440ae21bf0f589ae
REZ3X <rezex.dev01@gmail.com>
2025-04-27 18:21:49 +0700

    Adding assetPrefix and content load

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://ipv4.rejaka.me' : '',
+  
   images: {
     domains: ['ipv4.rejaka.me', 'rejaka.me'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
