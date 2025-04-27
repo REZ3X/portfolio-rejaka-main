13157b2c96538033cefb1f697963a7d8de9cdc71
REZ3X <rezex.dev01@gmail.com>
2025-04-27 17:59:57 +0700

    Adding domain config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['ipv4.rejaka.me', 'rejaka.me'],
+  },
 };
 
 export default nextConfig;
