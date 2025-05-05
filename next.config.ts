4afe22c50144eaa762a577e8e62f07d7eef7643a
REZ3X <rezex.dev01@gmail.com>
2025-05-05 13:15:13 +0700

    Reverting to one domain setting

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://ipv4.rejaka.me' : '',
-  
-  images: {
-    domains: ['ipv4.rejaka.me', 'rejaka.me'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
